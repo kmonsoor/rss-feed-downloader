983cd6bb893938876ca678448389748a8dc467b8
kmonsoor <kmonsoor@users.noreply.github.com>
2014-04-08 11:11:11 +0600

    ++ some sample header

--- test/test_on_win.py
+++ test/test_on_win.py
@@ -1,3 +1,13 @@
+#!/usr/local/bin/python2.7
+
+__project__ = "RSS-feed-downloader"
+__author__  = "Khaled Monsoor <k@kmonsoor.com>"
+__license__ = "MIT"
+
+"""
+This is a sample test module
+"""
+
 remote = "http://archive.apache.org/dist/apr/binaries/win32/apr-1.2.12-winnt-x86-ipv6-msvcrt60.zip"
 local = "c:\\new"
 print remote
